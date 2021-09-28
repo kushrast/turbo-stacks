345c8e7c2b7e2db481cfe2a29d9d9731dd6520d0
Kush Rastogi <kushrastogi@Kushs-MacBook-Pro.local>
2021-09-27 18:20:33 -0700

    Reactifying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {Heading, Icon} from 'react-bulma-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLiraSign, faSlidersH } from '@fortawesome/free-solid-svg-icons'
+import 'bulma/css/bulma.min.css';
+
+function Header(props) {
+  const style = {
+    paddingRight: "30px"
+  };
+
+  return (
+    <div class="header">          
+      <h1 class="title">
+        <span class="icon-text">
+          <span class="icon has-text-info">
+            <FontAwesomeIcon icon={faSlidersH} size="lg"/>
+          </span>
+          <span style={{"paddingLeft": "10px"}}>Turbo Stacks</span>
+        </span>
+      </h1>
+    </div>
+    );
+}
+
+function ListCard(props) {
+  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="section">
+        <div class="container">
+          <Header/>
+        </div>
+      </section>
     </div>
   );
 }
