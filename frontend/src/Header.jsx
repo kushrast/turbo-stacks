74af58ca0f1ba57e1afd1a7efa6f6feb7d3a1edc
Kush Rastogi <kushrastogi@Kushs-MBP.attlocal.net>
2021-10-08 18:12:13 -0700

    Making basic list app

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripHorizontal } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMouseOverIcon: false
+    };
+  }
+
+  toggleIsMouseOverIcon = () => {
+    this.setState({isMouseOverIcon: !this.state.isMouseOverIcon});
+  }
+
+  getIconColor = () => {
+    if (this.state.isMouseOverIcon) {
+      return "#D4EBD2";
+    } 
+    return "#DDDDDD";
+  }
+
+  render () {
+    return (
+      <div>
+        <div class="columns is-centered">          
+          <div class="column is-1 has-text-centered">
+            <FontAwesomeIcon icon={faGripHorizontal} size="5x" color={this.getIconColor()} onMouseEnter={this.toggleIsMouseOverIcon} onMouseLeave={this.toggleIsMouseOverIcon}/>
+          </div>
+        </div>
+      </div>
+      );
+  }
+}
+
+export default Header;
